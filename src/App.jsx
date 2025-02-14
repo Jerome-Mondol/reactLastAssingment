f51f9688496eba4a235b798c19e65da400b8ea2f
Jerome Mondol <jeromemondol11@gmail.com>
2025-02-14 16:53:33 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'remixicon/fonts/remixicon.css'
+
 
 const App = () => {
-  return (
-    <div>
 
-    </div>
+    const [search, setSearch] = useState("");
+
+    console.log("reloaded")
+
+
+  return (
+    <main>
+        <div className="navBar flex justify-between items-center p-7 border border-bottom border-gray-300 pt-12">
+            <div className="logo">
+                <img src="./logo.png" alt="" />
+            </div>
+            <div className="search w-[50%] rounded-4xl flex justify-between items-center p-1 border border-gray-300">
+                <input type="text"
+                    className="w-[90%] h-10 pl-5 outline-none"
+                    value={search}
+                    placeholder='restaurant, hotel, service...    |   Singapore'
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <div className="searchIcon bg-[#1677BD] rounded-4xl flex h-10 w-10 justify-center items-center">
+                    <i className="ri-search-line text-white"></i>
+                </div>
+            </div>
+            <div className="feedback flex gap-5 items-center">
+                <i class="ri-global-line text-2xl"></i>
+                <button className="bg-black px-7 py-3 text-white rounded-4xl cursor-pointer" >MyFeedback for business</button>
+            </div>
+        </div>
+    </main>
   )
 }
 
---
+++ src/components/NavDown.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NavDown = () => {
+
+    const icons = [
+        {icon: <i class="ri-layout-grid-line"></i>},
+        {icon: <i class="ri-restaurant-2-line"></i>},
+        {icon: <i class="ri-hotel-line"></i>},
+        {icon: <i class="ri-home-5-line"></i>},
+        {icon: <i class="ri-shopping-bag-line"></i>},
+        {icon: <i class="ri-car-line"></i>},
+        {icon: <i class="ri-brush-line"></i>},
+        {icon: <i class="ri-parent-fill"></i>},
+        {icon: <i class="ri-album-fill"></i>},
+        {icon: <i class="ri-car-washing-line"></i>},
+        {icon: <i class="ri-database-fill"></i>},
+        {icon: <i class="ri-weight-line"></i>}
+    ]
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default NavDown
